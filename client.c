961d722bad50217976f19cf12c6795dc3eb29610
Thanh <nguyentc@mail.gvsu.edu>
2023-11-14 19:05:12 -0500

    main

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+
+}
---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <strings.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+
+    return 0;
+
+}
