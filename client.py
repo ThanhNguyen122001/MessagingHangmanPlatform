682c6aea3505326dc20109fc6071da7afde78bf2
nguyenthanh <thanhnhotn2001@gmail.com>
2023-12-02 18:11:15 -0500

    Most Done, Need Testing

--- client.py
+++ client.py
@@ -1,20 +1,13 @@
 import socket
 import sys
 
-def client():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a Socket
-    host = 'localhost' # Asks for IP Address
-    port = 1234
+#Client Implementation
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a Socket
+host = 'localhost' # Asks for IP Address
+port = 1234
     
-    sock.connect((host, port)) # Connects to Server
+sock.connect((host, port)) # Connects to Server
     
-    try:
-        while True:
-            userInput = input()
-            sock.send(bytes(userInput, 'UTF-8'))
-            print("Message Sent")
-    except KeyboardInterrupt:
-        sock.close()
-        
-if __name__ == "__main__":
-    client()
+userInput = input()
+sock.send(bytes(userInput, 'UTF-8'))
+print("Message Sent")
--- server.py
+++ server.py
@@ -1,126 +1,173 @@
 import sys
 import socket
 
-guessingLetter = ''
+#Server Implementation 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a Socket
+host = 'localhost' # Asks for IP Address
+port = 1234
+    
+sock.bind((host, port)) # Binds the sockets
+sock.listen(5) # Maximum of Connections
+    
+while True:
+    connection, address = sock.accept() # Accept Connection
+    buffer = connection.recv(1024)
+    print(str(buffer, "utf-8"))
+    guessingLetter = str(buffer, "utf-8")
+    break
+    
+print(guessingLetter)
 
-def server():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a Socket
-    host = 'localhost' # Asks for IP Address
-    port = 1234
-    
-    sock.bind((host, port)) # Binds the sockets
-    sock.listen(5) # Maximum of Connections
-    
-    while True:
-        connection, address = sock.accept() # Accept Connection
-        buffer = connection.recv(1024)
-        print(str(buffer, "utf-8"))
-        guessingLetter = str(buffer, "utf-8")
-        break
-    
-    print(guessingLetter)
+# Hangman Drawings
+drawings = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
-
-def game():
-    inputLetter = '' #This is the input from the players.
-    mainWord = '' #The word the players are guessing
-    # blankWord = main
-    index = 0
-    wrongCount = 0
-    indexArray = []
-    gameStatus = True
+def board(drawings, missLetters, correctLetters, mainWord):
+    print(drawings[len(missLetters)]) # Printing the drawing depends how many letters are missed 
+    print() # Prints a new line
     
-    #Ask for Winning game input
-    mainWord = input("Enter your Word: ")
-    print(mainWord)
+    print('All Missed Letters: ', end='')
+    for l in missLetters:
+        print(l, end='')
+    print()
+    indexArray = [] #This will hold the index values of the 
     
-    #Print out the current drawing of hangman
-    print(hangmanDrawing(wrongCount))
+    emptyLetters = '_' * len(mainWord) # Prints '_' how many times depends on the length of the 'mainWord"
     
-    while gameStatus:
-        #print("Did we get here?")
-        #Ask for inputLetter
-        inputLetter = guessingLetter
-        
-        if wrongCount == 7:
-            gameStatus = False
-        
-        if mainWord.__contains__(inputLetter):
-            for character in mainWord:
-                if(character == inputLetter):
-                    #append to the outputword
-                    #print(index) #This will print out the index of the character that we are looking for.
-                    indexArray.append(index)
-                index = index + 1
-            index = 0
-            #Now add in the word with blanks
-        else:
-            wrongCount = wrongCount + 1
-            print(f"Total Wrongs: {wrongCount}")
-        
-        for char in mainWord:#printing out the word with spaces
-            if(indexArray.__contains__(index)):
-                print (inputLetter, end="")
-            else:
-                print("_", end="")
-            index = index + 1
-        print(hangmanDrawing(wrongCount))
-        
-           
+    # Filling the empty Letters
+    for c in range(len(mainWord)):
+        if mainWord[c] in correctLetters:
+            emptyLetters = emptyLetters[:c] + mainWord[c] + emptyLetters[c+1:]
             
-def hangmanDrawing(index):
-    drawings = ['''
-        +---+
-         |  |
-            |
-            |
-            |
-            |
-        =========''', '''
-        +---+
-         |  |
-         O  |
-            |
-            |
-            |
-        =========''', '''
-        +---+
-         |  |
-         O  |
-         |  |
-            |
-            |
-        =========''', '''
-        +---+
-         |  |
-         O  |
-        /|  |
-            |
-            |
-        =========''', '''
-         +---+
-         |   |
-         O   |
-        /|\  |
-             |
-             |
-        =========''', '''
-         +---+
-         |   |
-         O   |
-        /|\  |
-        /    |
-             |
-        =========''', '''
-         +---+
-         |   |
-         O   |
-        /|\  |
-        / \  |
-             |
-        =========''']
-    return drawings[index]
+    # Printing out the work
+    for let in emptyLetters:
+        print(let, end='')
+    print()
+            
+    
+#def guess(alreadyGuess, gameStatus):
+#    while gameStatus:
+#        #print("Did we get here?")
+#        #Ask for inputLetter
+#        inputLetter = guessingLetter
+#        index = 0       
+#        if mainWord.__contains__(inputLetter):
+#            for character in mainWord:
+#                if(character == inputLetter):
+#                    #append to the outputword
+#                    #print(index) #This will print out the index of the character that we are looking for.
+#                    indexArray.append(index)
+#                index = index + 1
+            #Now add in the word with blanks
+#        else:
+#            wrongCount = wrongCount + 1
+#            print(f"Total Wrongs: {wrongCount}")
+        
+def letterChecker(guess, guessedLetters):
+    if guess.isalpha():
+        # input checks
+        if len(guess) != 1: # Checks Size of String
+            guess = input("Enter your guessing letter: ")
+            guess = guess.lower() 
+        elif inputLetter in guessedLetters: # Checks if Letter have been guessed or not
+            guess = input("Enter a letter that hasn't been guessed: ")
+            guess = guess.lower()
+    else:
+        guess = input("Please enter an actual Letter: ")
+        guess = guess.lower()
+        
+    return guess
 
-if __name__ == "__main__":
-    game()
-    server()
+mainWord = input("Enter your secret word: ") #The word the players are guessing
+print(mainWord) #Printing "mainWord" to server terminal to check if it working
+inputLetter = '' #This is the input from the players.
+missLetters = '' #All the letter that the user missed.
+correctLetters = '' #All the letters that the usermgot correct.
+gameStatus = True # Current Game Status
+while True:
+    #Displaying the game board
+    board(drawings, missLetters, correctLetters, mainWord)
+    
+    # Obtaining the input  
+    inputLetter = input("Enter your guessing letter: ")
+    inputLetter = inputLetter.lower() # Make any letter is lowercase
+    inputLetter = letterChecker(inputLetter, missLetters + correctLetters)
+        
+    if inputLetter in mainWord:
+        correctLetters = correctLetters + inputLetter
+        completeWord = True # Status if word has been found
+        
+        #If player hasn't found all of the letters
+        for c in range(len(mainWord)):
+            if mainWord[c] not in correctLetters:
+                correctLetters = False
+                break
+        
+        # If player found all of the letters 
+        if completeWord == True:
+            print("All the Letters has been found!!")
+            print("You Win")
+            gameStatus = True
+            break
+        
+    else:
+        missLetters = missLetters + inputLetter
+        # No more guesses are  possible 
+        if len(missLetters) == len(drawings) - 1:
+            board(drawings, missLetters, correctLetters, mainWord) #Print out the final board
+            print("No more guesses\n")
+            print("Game Over, You Lose")
+            gameStatus = False
+            break
+    
+
+#Finish with the game close the server
+sock.close()
